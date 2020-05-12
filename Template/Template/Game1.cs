67e188b17a6d5ef16c86c778bf9583184db61de6
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2020-05-12 13:52:11 +0200

    startskärm, spelskärm, slutskärm

--- Template/Template/Game1.cs
+++ Template/Template/Game1.cs
@@ -17,18 +17,22 @@ namespace Template
         public static int hojd;
 
         private List<Sprite> _sprites;
-        private Texture2D _texture;
+        public Texture2D _texture;
         private Vector2 _position;
         private float speed = 30f;
 
         private Texture2D _background;
         private Vector2 _backgroundpos = new Vector2(0, 0);
+        private Texture2D texture;
 
         private Texture2D _bullettexture;
         private Vector2 _bulletposition;
 
         private int screen = 0;
 
+        private SpriteFont font;
+        private float countdown = 0;
+        
         
 
         public Game1()
@@ -52,7 +56,7 @@ namespace Template
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            var texture = Content.Load<Texture2D>("ak-47");
+            texture = Content.Load<Texture2D>("ak-47");
 
             _bullettexture = Content.Load<Texture2D>("bullet");
 
@@ -81,6 +85,7 @@ namespace Template
             };
 
             _background = Content.Load<Texture2D>("dust2");
+            font = Content.Load<SpriteFont>("Countdown");
 
             _bullettexture = Content.Load<Texture2D>("bullet");
             _bulletposition = new Vector2(0, 0);
@@ -109,19 +114,28 @@ namespace Template
                 if (_sprites[1].Bullet != null && _sprites[0].Size.Intersects(_sprites[1].Bullet.Bullethitbox))
                 {
                     _sprites[0].Alive = false;
+                    screen = 2;
                 }
                 else if (_sprites[0].Bullet != null && _sprites[1].Size.Intersects(_sprites[0].Bullet.Bullethitbox))
                 {
                     _sprites[1].Alive = false;
+                    screen = 2;
                 }
-            } 
+            }
             else if (screen == 2) //Slutskärm
             {
-
+                if (Keyboard.GetState().IsKeyDown(Keys.RightShift))
+                {
+                    screen = 0;
+                }
             }
             else if (screen == 0) //Startskärm
             {
-
+                countdown += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (countdown > 3)
+                {
+                    screen = 1;
+                }
             }
                 base.Update(gameTime);
 
@@ -136,13 +150,19 @@ namespace Template
             backgroundRec.Location = _backgroundpos.ToPoint();
             backgroundRec.Size = new Point(bredd, hojd);
             spriteBatch.Draw(_background, backgroundRec, Color.White);
-            if (screen == 1) 
+            if (screen == 1) //Spelskärm
             {
-
-
-
                 foreach (var sprite in _sprites)
                     sprite.Draw(spriteBatch);
+            }
+            else if (screen == 2) //Slutskärm
+            {
+                spriteBatch.DrawString(font, ("Press enter for restart"), new Vector2(560, 190), Color.Black);
+                spriteBatch.DrawString(font,("Player Won"), new Vector2(660, 450), Color.Black);
+            }
+            else if (screen == 0) //Startskärm
+            {
+                spriteBatch.DrawString(font,(3-(int)countdown).ToString(), new Vector2(960, 390), Color.Black);
             }
             spriteBatch.End();
 
